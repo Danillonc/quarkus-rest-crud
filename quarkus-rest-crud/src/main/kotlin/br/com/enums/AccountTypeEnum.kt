e07a42bea71434e2bfc8cb7a36b379a29c94927b
Danillo <danillonc@live.com>
2020-06-02 17:49:42 -0300

    Starting a project quarkus config first commit

---
+++ quarkus-rest-crud/src/main/kotlin/br/com/enums/AccountTypeEnum.kt
@@ -0,0 +1,9 @@
+package br.com.enums
+
+/**
+ * Enum representa a account type in bank.
+ */
+enum class AccountTypeEnum {
+    PF,
+    PJ
+}
---
+++ quarkus-rest-crud/src/main/kotlin/br/com/service/impl/AccountBankServiceImpl.kt
@@ -0,0 +1,4 @@
+package br.com.service.impl
+
+class AccountBankServiceImpl {
+}
